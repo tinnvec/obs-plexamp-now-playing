0862af5d79eb849f79328a2675288578dd7b5cff
Alex Hanna <tinnvec@gmail.com>
2022-07-02 11:35:22 -0500

    Update default settings

--- config/~settings.js
+++ config/~settings.js
@@ -1,12 +1,29 @@
+// --- Plex Settings ---
+
+// Plex hostname might be an IP address or URL depending on your setup
+export const PLEX_HOSTNAME = "192.168.1.100";
+
+// Plex port default is 32400
+export const PLEX_PORT = 32400;
+
+// Use https for Plex connection, default is false
+export const PLEX_HTTPS = false;
+
+// Plex token, to obtain see: https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
+export const PLEX_TOKEN = "";
+
+// --- Output Settings ---
+
+// Output mode: text or html
+export const OUTPUT_MODE = "text";
+
 // Filename for text output
 export const OUTPUT_TEXT_FILENAME = "nowplaying.txt";
 
-// Location of PlayQueue.json, make sure to use double backslashes (\\) on Windows.
-// Often something like the following:
-// Windows: C:\\Users\\USERNAME\\AppData\\Local\\Plexamp\\Plexamp
-export const PLAY_QUEUE_LOCATION = "C:\\Users\\USERNAME\\AppData\\Local\\Plexamp\\Plexamp";
+// String to add to the end of text output
+export const TEXT_SEPARATOR = " | ";
+
+// --- App Settings ---
 
 // Rate to read play queue file, in milliseconds
 export const POLLING_RATE = 1000;
-
-export const TEXT_SEPARATOR = " | ";
