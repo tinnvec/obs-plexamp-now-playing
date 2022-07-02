d20c7dfcd51b60069a44e8fae7c7742af16da7c2
Alex Hanna <tinnvec@gmail.com>
2022-07-01 21:49:29 -0500

    Add default settings

---
+++ config/~settings.js
@@ -0,0 +1,12 @@
+// Filename for text output
+export const OUTPUT_TEXT_FILENAME = "nowplaying.txt";
+
+// Location of PlayQueue.json, make sure to use double backslashes (\\) on Windows.
+// Often something like the following:
+// Windows: C:\\Users\\USERNAME\\AppData\\Local\\Plexamp\\Plexamp
+export const PLAY_QUEUE_LOCATION = "C:\\Users\\USERNAME\\AppData\\Local\\Plexamp\\Plexamp";
+
+// Rate to read play queue file, in milliseconds
+export const POLLING_RATE = 1000;
+
+export const TEXT_SEPARATOR = " | ";
